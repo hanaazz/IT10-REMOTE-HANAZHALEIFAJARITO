b096b7ca3db9b5fc3face2d9168939a7c2060b3c
Hana <hanaazz2121@gmail.com>
2025-05-14 17:26:05 +0800

    Implement font size change on selection

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ fontSelect.addEventListener('change', function (){
     const selectedFont=fontSelect.value;
     paragraph.style.fontFamily=selectedFont;
 });
+
+const sizeSelect=document.getElementById('sizeSelect');
+const paragraphs=document.querySelector('p');
+
+sizeSelect.addEventListener('change', function () {
+    const selectedSize=sizeSelect.value;
+    paragraphs.style.fontSize=selectedSize;
+})
